f280136f90479f0a616320a5f59e890b32139e9a
Mustafa Yasin HALICI <yasin@sudo.co>
2017-04-02 18:26:10 +0300

    add to excel

--- main.go
+++ main.go
@@ -7,6 +7,8 @@ import (
 	"net/http"
 	"strconv"
 	"strings"
+
+	"github.com/tealeg/xlsx"
 )
 
 type Brands []struct {
@@ -54,9 +56,9 @@ func getBrands() {
 	brandsresult, err := bodyBrands([]byte(body))
 
 	for i := 0; i < len(*brandsresult); i++ {
-		fmt.Println((*brandsresult)[i].Text)
-		fmt.Println((*brandsresult)[i].Value)
-		getModels((*brandsresult)[i].Value)
+		// fmt.Println((*brandsresult)[i].Text)
+		// fmt.Println((*brandsresult)[i].Value)
+		getModels((*brandsresult)[i].Value, (*brandsresult)[i].Text)
 	}
 }
 
@@ -68,7 +70,7 @@ func bodyModels(body []byte) (*Models, error) {
 	}
 	return s, err
 }
-func getModels(brand int64) {
+func getModels(brand int64, brandname string) {
 	res, err := http.Get("https://www.hum.com/bin/core/findPort." + strconv.FormatInt(brand, 10) + ".json")
 	hodo(err)
 	body, err := ioutil.ReadAll(res.Body)
@@ -76,9 +78,9 @@ func getModels(brand int64) {
 	modelsresult, err := bodyModels([]byte(body))
 
 	for i := 0; i < len(*modelsresult); i++ {
-		fmt.Println((*modelsresult)[i].Text)
-		fmt.Println((*modelsresult)[i].Value)
-		getModelYears(brand, (*modelsresult)[i].Value)
+		// fmt.Println((*modelsresult)[i].Text)
+		// fmt.Println((*modelsresult)[i].Value)
+		getModelYears(brand, (*modelsresult)[i].Value, brandname, (*modelsresult)[i].Text)
 	}
 }
 
@@ -90,7 +92,7 @@ func bodyModelYears(body []byte) (*ModelYears, error) {
 	}
 	return s, err
 }
-func getModelYears(brand int64, model int64) {
+func getModelYears(brand int64, model int64, brandname string, modelname string) {
 	res, err := http.Get("https://www.hum.com/bin/core/findPort." + strconv.FormatInt(brand, 10) + "." + strconv.FormatInt(model, 10) + ".json")
 	hodo(err)
 	body, err := ioutil.ReadAll(res.Body)
@@ -98,8 +100,8 @@ func getModelYears(brand int64, model int64) {
 	modelyearsresult, err := bodyModelYears([]byte(body))
 
 	for i := 0; i < len(*modelyearsresult); i++ {
-		fmt.Println((*modelyearsresult)[i])
-		getModelChasis(brand, model, (*modelyearsresult)[i])
+		// fmt.Println((*modelyearsresult)[i])
+		getModelChasis(brand, model, (*modelyearsresult)[i], brandname, modelname)
 	}
 }
 
@@ -111,7 +113,7 @@ func bodyModelChasis(body []byte) (*ModelChasis, error) {
 	}
 	return s, err
 }
-func getModelChasis(brand int64, model int64, modelyears int64) {
+func getModelChasis(brand int64, model int64, modelyears int64, brandname string, modelname string) {
 	res, err := http.Get("https://www.hum.com/bin/core/findPort." + strconv.FormatInt(brand, 10) + "." + strconv.FormatInt(model, 10) + "." + strconv.FormatInt(modelyears, 10) + ".json")
 	hodo(err)
 	body, err := ioutil.ReadAll(res.Body)
@@ -119,9 +121,9 @@ func getModelChasis(brand int64, model int64, modelyears int64) {
 	modelchasisresult, err := bodyModelChasis([]byte(body))
 
 	for i := 0; i < len(*modelchasisresult); i++ {
-		fmt.Println((*modelchasisresult)[i].Text)
-		fmt.Println((*modelchasisresult)[i].Value)
-		getModelVersions(brand, model, modelyears, (*modelchasisresult)[i].Value)
+		// fmt.Println((*modelchasisresult)[i].Text)
+		// fmt.Println((*modelchasisresult)[i].Value)
+		getModelVersions(brand, model, modelyears, (*modelchasisresult)[i].Value, brandname, modelname, (*modelchasisresult)[i].Text)
 	}
 }
 
@@ -133,7 +135,7 @@ func bodyModelVersions(body []byte) (*ModelVersions, error) {
 	}
 	return s, err
 }
-func getModelVersions(brand int64, model int64, modelyears int64, modelchasis int64) {
+func getModelVersions(brand int64, model int64, modelyears int64, modelchasis int64, brandname string, modelname string, modelchasisname string) {
 	res, err := http.Get("https://www.hum.com/bin/core/findPort." + strconv.FormatInt(brand, 10) + "." + strconv.FormatInt(model, 10) + "." + strconv.FormatInt(modelyears, 10) + "." + strconv.FormatInt(modelchasis, 10) + ".json")
 	hodo(err)
 	body, err := ioutil.ReadAll(res.Body)
@@ -141,9 +143,9 @@ func getModelVersions(brand int64, model int64, modelyears int64, modelchasis in
 	modelversionresult, err := bodyModelVersions([]byte(body))
 
 	for i := 0; i < len(*modelversionresult); i++ {
-		fmt.Println((*modelversionresult)[i].Text)
-		fmt.Println((*modelversionresult)[i].Value)
-		getLocation(brand, model, modelyears, modelchasis, (*modelversionresult)[i].Value)
+		// fmt.Println((*modelversionresult)[i].Text)
+		// fmt.Println((*modelversionresult)[i].Value)
+		getLocation(brand, model, modelyears, modelchasis, (*modelversionresult)[i].Value, brandname, modelname, modelchasisname, (*modelversionresult)[i].Text)
 	}
 }
 
@@ -155,18 +157,51 @@ func bodyLocation(body []byte) (*Location, error) {
 	}
 	return s, err
 }
-func getLocation(brand int64, model int64, modelyears int64, modelchasis int64, modelversion int64) {
+func getLocation(brand int64, model int64, modelyears int64, modelchasis int64, modelversion int64, brandname string, modelname string, modelchasisname string, modelversionname string) {
 	res, err := http.Get("https://www.hum.com/bin/core/findPort." + strconv.FormatInt(brand, 10) + "." + strconv.FormatInt(model, 10) + "." + strconv.FormatInt(modelyears, 10) + "." + strconv.FormatInt(modelchasis, 10) + "." + strconv.FormatInt(modelversion, 10) + ".json")
 	hodo(err)
 	body, err := ioutil.ReadAll(res.Body)
 	hodo(err)
 	locationresult, err := bodyLocation([]byte(body))
 
-	fmt.Println(locationresult.Location)
-	fmt.Println(locationresult.Picture)
-	fmt.Println(strings.Replace(locationresult.Area, "/media/", "https://www.hum.com/content/dam/", -1))
+	carcounter++
+	row = sheet.AddRow()
+	cell = row.AddCell()
+	cell.Value = brandname
+	cell = row.AddCell()
+	cell.Value = modelname
+	cell = row.AddCell()
+	cell.Value = strconv.FormatInt(modelyears, 10)
+	cell = row.AddCell()
+	cell.Value = modelchasisname
+	cell = row.AddCell()
+	cell.Value = modelversionname
+	cell = row.AddCell()
+	cell.Value = locationresult.Location
+	cell = row.AddCell()
+	cell.Value = locationresult.Picture
+	cell = row.AddCell()
+	cell.Value = strings.Replace(locationresult.Area, "/media/", "https://www.hum.com/content/dam/", -1)
+	err = file.Save("obdVampire.xlsx")
+	hodo(err)
+
+	fmt.Println("Car count: ", carcounter)
+	// fmt.Println(locationresult.Location)
+	// fmt.Println(locationresult.Picture)
+	// fmt.Println(strings.Replace(locationresult.Area, "/media/", "https://www.hum.com/content/dam/", -1))
 }
 
+var file *xlsx.File
+var sheet *xlsx.Sheet
+var row *xlsx.Row
+var cell *xlsx.Cell
+var carcounter = 0
+
 func main() {
+	var err error
+	file = xlsx.NewFile()
+	sheet, err = file.AddSheet("obdVampire")
+	hodo(err)
+	row = sheet.AddRow()
 	getBrands()
 }
